7fc64dc2d8086eb9c931cbc8306fd38e0e28b60a
Deibit <doliva@refineriaweb.com>
2017-08-18 20:09:19 +0200

    Added checks to the links

--- linkScripts/checkLinks.py
+++ linkScripts/checkLinks.py
@@ -5,7 +5,31 @@ import urllib2
 import re
 import requests
 
+def isSocialLink(url):
+    socialUrls = ['facebook', 'twitter']
+    for socialUrl in socialUrls:
+        if socialUrl in url:
+            return True
+
+def isMailLink(url):
+    return 'mailto' in url.lower()
+
+def isImageLink(url):
+    # check extensions
+    extensions = ['jpeg', 'jpg', 'png']
+    urlArray = url.split('.')
+    count = len(urlArray)
+    return urlArray[count -1] not in extensions
+
+def isValidLink(url):
+    if not isSocialLink(url) and not isMailLink(url) and not isImageLink(url):
+        return True
+    return False
+
+
 def getLinks(url, result, base):
+    if isValidLink(url):
+        return result
     try:
         # get page
         html_page = urllib2.urlopen(url)
@@ -28,7 +52,7 @@ def getLinks(url, result, base):
     # foreach link in page
     for link in links:
         if '#' in link.get('href'):
-            print "Found '#' on ", url
+            print "Found '#' on ", url, '(',link.get('href'),')'
         if base in link.get('href') and link.get('href') not in result and link.get('href') != None:
             result.append(link.get('href'))
             result.extend(getLinks(link.get('href'), result, base))
